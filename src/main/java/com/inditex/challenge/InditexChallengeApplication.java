ac8ab5c5081b49e01433901de8c2fc2ae0842a17
Alejandro Cabo <alejandro.cabo@lucual18.es>
2024-03-14 14:13:23 +0100

    Minor Refactor

--- src/main/java/com/inditex/challenge/InditexChallengeApplication.java
+++ src/main/java/com/inditex/challenge/InditexChallengeApplication.java
@@ -1,6 +1,5 @@
 package com.inditex.challenge;
 
-import lombok.Value;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
@@ -9,19 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InditexChallengeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InditexChallengeApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(InditexChallengeApplication.class, args);
 
-		Logger logger = LogManager.getLogger(InditexChallengeApplication.class);
-		logger.info("Application started");
-		logger.info("===========================================================================================");
-		logger.info("You can access the API documentation at http://localhost:8080/swagger-ui.html");
-		logger.info("===========================================================================================");
-		logger.info("Check H2 database in URL: http://localhost:8080/h2-console with the following parameters:");
-		logger.info("Username: sa");
-		logger.info("Password: password");
-		logger.info("===========================================================================================");
+        Logger logger = LogManager.getLogger(InditexChallengeApplication.class);
+        logger.info("===========================================================================================");
+        logger.info("Application started :)");
+        logger.info("===========================================================================================");
+        logger.info("Check H2 database in URL: http://localhost:8080/h2-console with the following parameters:");
+        logger.info("Username: sa");
+        logger.info("Password: password");
+        logger.info("===========================================================================================");
 
-	}
+    }
 
 }
