d0688d2cd622c160a5f4c7906565117f421a4819
Alejandro Cabo <alejandro.cabo@lucual18.es>
2024-04-04 19:03:46 +0200

    more stuff

--- src/test/java/com/inditex/challenge/Controller/ServerStatusTest.java
+++ src/test/java/com/inditex/challenge/Controller/ServerStatusTest.java
@@ -1,41 +1,27 @@
 package com.inditex.challenge.Controller;
 
-import com.inditex.challenge.controller.api.ServerStatus;
-import com.inditex.challenge.dto.ResponseDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
-import java.util.Objects;
-import java.util.logging.Logger;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static java.lang.String.format;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = "spring.sql.init.data-locations=classpath:/no-data.sql")
 public class ServerStatusTest {
 
-    Logger logger = Logger.getLogger(ServerStatusTest.class.getName());
+    @LocalServerPort
+    private int port;
 
     @Autowired
-    private ServerStatus serverStatus;
-
-    @Test
-    void contextLoads() {
-        assertThat(serverStatus).isNotNull();
-    }
+    private TestRestTemplate restTemplate;
 
     @Test
     void greetingShouldReturnDefaultMessage() {
-        ResponseEntity<ResponseDTO> result = this.serverStatus.hello();
-
-        logger.info("Response: " + result.toString());
-
-        assertSame(result.getStatusCode(), HttpStatus.OK);
-        assertSame(Objects.requireNonNull(result.getBody()).statusMessage, "OK");
-        assertSame(result.getBody().message, "All good");
-
+        assertThat(this.restTemplate.getForObject(format("http://localhost:%d/status", port), String.class)).contains("All good");
     }
 }
